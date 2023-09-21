40d325d9c3b3e372a44e61aa1cd9be2661a8dce0
Anthony DOUCHET <anthony.douchet@labom2iformation.fr>
2023-09-21 14:36:41 +0200

    Création des Models + Data + PasswordValidator

---
+++ AntManZooClassLibrary/Models/Staff.cs
@@ -0,0 +1,34 @@
+﻿using AntManZooClassLibrary.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntManZooClassLibrary.Models
+{
+    public class Staff
+    {
+        [Required]
+
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z].*", ErrorMessage = "FirstName must start with an Uppercase Letter !")]
+        public string? Firstname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z\-]*", ErrorMessage = "LastName must be only Uppercase !")]
+        public string? Lastname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+
+        [Required]
+        [PasswordValidator]
+        public string? Password { get; set; }
+
+    }
+}
