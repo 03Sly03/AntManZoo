86c230d359f421d872a641e6334a704767c630db
Sly03 <slydevwebproject@outlook.fr>
2023-09-22 12:08:33 +0200

    feature/Add DetailsAnimal page

--- AntManZooBlazor/Pages/DetailsAnimal.razor.cs
+++ AntManZooBlazor/Pages/DetailsAnimal.razor.cs
@@ -2,21 +2,24 @@
 using AntManZooClassLibrary.Models;
 using Microsoft.AspNetCore.Components;
 
+
 namespace AntManZooBlazor.Pages
 {
-    public partial class Index
+    public partial class DetailsAnimal
     {
+        private string? LoadingMessage { get; set; }
+        private Animal? MyAnimal { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
         [Inject]
         public IAnimalService AnimalService { get; set; }
-        private string? LoadingMessage { get; set; }
-        private List<Animal> AnimalsList { get; set; } = new List<Animal>();
 
         protected override async Task OnInitializedAsync()
         {
-            LoadingMessage = "Récupération des données des animaux";
-            AnimalsList = await AnimalService.GetAll();
+            LoadingMessage = "Récupération des données de l'animal";
+            MyAnimal = await AnimalService.Get(Id);
             LoadingMessage = "";
         }
-        
     }
 }
