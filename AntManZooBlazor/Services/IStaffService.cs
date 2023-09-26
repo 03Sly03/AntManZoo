20dcf364d5ce4168d3888445f1ffd2c40fe3cca0
Sly03 <slydevwebproject@outlook.fr>
2023-09-26 13:47:45 +0200

    fix/register staff bdd

--- AntManZooBlazor/Services/IStaffService.cs
+++ AntManZooBlazor/Services/IStaffService.cs
@@ -9,7 +9,7 @@ namespace AntManZooBlazor.Services
         Task<List<Staff>> GetAll();
         Task<bool> PostLogin(LoginRequestDTO staffDTO);
         Task<bool> Post(Staff staff);
-        Task<bool> Put(Staff staff);
-        Task<bool> Delete(int id);
+        //Task<bool> Put(Staff staff);
+        //Task<bool> Delete(int id);
     }
 }
